03a5bda184e3cb71ed2dca5686a849a785f205a5
rynxiao <yuzhongzi91@sina.com>
2020-09-06 18:01:29 +0800

    feat: using babel transform func

--- babel-core/index.js
+++ babel-core/index.js
@@ -10,6 +10,7 @@ const filename = 'index.ts.js'
 const { ast } = babel.transformSync(source, { filename, ast: true, code: false });
 const { code, map } = babel.transformFromAstSync(ast, source, {
     filename,
+    plugins: ["@babel/plugin-transform-arrow-functions", "@babel/plugin-transform-parameters"],
     presets: ["minify"],
     babelrc: false,
     configFile: false,
