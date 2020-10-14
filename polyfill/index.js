3ed65e6ed08609bab4b7ed9d1464b55f5a762587
rynxiao <yuzhongzi91@sina.com>
2020-10-14 10:54:13 +0800

    feat: add Edge finally polyfill test

--- polyfill/index.js
+++ polyfill/index.js
@@ -1,6 +1,22 @@
 const sym = Symbol();
 
 const promise = Promise.resolve();
+const promise1 = new Promise(((resolve, reject) => {
+  const num = Math.random() * 10;
+  if (num < 10) {
+    resolve(num);
+  } else {
+    reject('error');
+  }
+}));
+
+promise1.then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.error(err);
+}).finally(() => {
+  console.log('finally');
+});
 
 const arr = ["arr", "yeah!"];
 const check = arr.includes("yeah!");
